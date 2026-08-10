fd23896024efe35fbe6d2f6fa57c06f005335c93
jihanghaniyyah <jghaniyyah@gmail.com>
2026-08-10 18:49:40 +0700

    revisi kesekian kalinya huft

--- src/components/story/SearchHotspot.tsx
+++ src/components/story/SearchHotspot.tsx
@@ -9,9 +9,7 @@ interface SearchHotspotProps {
   hotspots: Hotspot[];
   target: string;
   wrongMessages: string[];
-
   cameraX: number;
-
   onSuccess: () => void;
 }
 
@@ -47,24 +45,24 @@ export default function SearchHotspot({
   return (
     <>
       <div
-        className="absolute left-0 top-0 h-full z-40 pointer-events-auto"
+        className="absolute left-0 top-0 z-40 h-full pointer-events-auto"
         style={{
           width: `${PANORAMA.WIDTH}px`,
           transform: `translateX(${cameraX}px)`,
         }}
       >
         {hotspots.map((spot) => (
-          <div key={spot.id}>
+          <div key={spot.id} className="absolute inset-0">
             {/* Ripple hanya untuk next_room */}
             {spot.id === "next_room" && (
               <div
                 className="
-      absolute
       pointer-events-none
+      absolute
       z-[999]
       flex
       items-center
-      gap-3
+      justify-center
     "
                 style={{
                   left: `calc(${spot.left} + 350px)`,
@@ -72,18 +70,44 @@ export default function SearchHotspot({
                   transform: "translateY(-50%)",
                 }}
               >
-                <span className="next-arrow delay-0">❯</span>
-                <span className="next-arrow delay-1">❯</span>
-                <span className="next-arrow delay-2">❯</span>
+                {/* Outer ripple */}
+                <span
+                  className="
+        absolute
+        h-20
+        w-20
+        rounded-full
+        border-[4px]
+        animate-ripple-breathe
+      "
+                  style={{
+                    borderColor: "#FF3B3B",
+                    boxShadow: "0 0 25px rgba(255, 59, 59, 0.95)",
+                  }}
+                />
+
+                {/* Inner ripple */}
+                <span
+                  className="
+        absolute
+        h-10
+        w-10
+        rounded-full
+        border-[3px]
+        animate-ripple-breathe
+      "
+                  style={{
+                    borderColor: "#FF5A5A",
+                    boxShadow: "0 0 15px rgba(255, 90, 90, 0.95)",
+                  }}
+                />
               </div>
             )}
 
+            {/* Area hotspot */}
             <button
-              onClick={() => handleClick(spot.id!)}
-              className="
-                absolute
-                cursor-pointer
-              "
+              onClick={() => handleClick(spot.id)}
+              className="absolute cursor-pointer"
               style={{
                 left: spot.left,
                 right: spot.right,
